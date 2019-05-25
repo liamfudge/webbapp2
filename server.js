784015a70d8509b4b0cd1ca1ef4e6da92330bd0f
liamfudge <liam.fudge@gmail.com>
2019-05-25 11:38:25 +1000

    Build server and add .ebextensions

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require('express');
+import path from 'path';
+import express from 'express';
 
 const PORT = process.env.HTTP_PORT || 4001;
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.get('/', (req, res) => {
 	res.send('flowers are cool');
