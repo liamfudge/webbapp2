248d0f2c9b71910dafe9f7c2143d77de47cf4244
liamfudge <liam.fudge@gmail.com>
2019-05-25 14:10:41 +1000

    i think this one will work

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client2', 'build')));
 
 app.get('/', (req, res) => {
-	res.send('flowers are cool');
+	res.send('flowers are not cool');
+	console.log('this is a server console.log');
 });
 
 app. get('/flower', (req, res) => {
